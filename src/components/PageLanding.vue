de356c42315fb4d0c5e635445d156bcd57816c07
Aob BKK <aob.wiriyagulpat@dx1app.com>
2018-03-19 18:32:55 +0700

    update gallery in landing page

--- src/components/PageLanding.vue
+++ src/components/PageLanding.vue
@@ -58,7 +58,7 @@
 
                 <div class="col-md-4 margin-t30">
                 <div class="portfolio-item">
-                    <a href="https://scontent.fbkk12-1.fna.fbcdn.net/v/t31.0-8/20121200_1445878972144996_1725100968484993448_o.jpg?oh=48994a51102b7ca810bd1f469d3a9547&oe=5B4BD504" data-lightbox="portfolio">
+                    <a href="../static/img/portfolio/item1.jpg" data-lightbox="portfolio">
                     <img src="../assets/img/portfolio/item1.jpg" alt="">
                     </a>
                 </div>
@@ -66,7 +66,7 @@
 
                 <div class="col-md-4 margin-t30">
                 <div class="portfolio-item">
-                    <a href="https://scontent.fbkk12-1.fna.fbcdn.net/v/t31.0-8/19944595_1736201119754187_1593587063392924517_o.jpg?oh=b6f5d9646d924bffae72c05e167ff4a4&oe=5B0B0900" data-lightbox="portfolio">
+                    <a href="../static/img/portfolio/item2.jpg" data-lightbox="portfolio">
                     <img src="../assets/img/portfolio/item2.jpg" alt="">
                     </a>
                 </div>
@@ -74,7 +74,7 @@
 
                 <div class="col-md-4 margin-t30">
                 <div class="portfolio-item">
-                    <a href="https://scontent.fbkk12-1.fna.fbcdn.net/v/t31.0-8/19942606_1731420190232280_1670578840384535054_o.jpg?oh=8d76cbbcc74ed13546dff6b44ee61dae&oe=5B1A12A4" data-lightbox="portfolio">
+                    <a href="../static/img/portfolio/item3.jpg" data-lightbox="portfolio">
                     <img src="../assets/img/portfolio/item3.jpg" alt="">
                     </a>
                 </div>
@@ -82,7 +82,7 @@
 
                 <div class="col-md-4 margin-t30">
                 <div class="portfolio-item">
-                    <a href="https://scontent.fbkk12-1.fna.fbcdn.net/v/t31.0-8/19575160_10212285609631948_2249457055390730749_o.jpg?oh=4f7886a9162f23674a9166c9b70ba16f&oe=5B18409A" data-lightbox="portfolio">
+                    <a href="../static/img/portfolio/item4.jpg" data-lightbox="portfolio">
                     <img src="../assets/img/portfolio/item4.jpg" alt="">
                     </a>
                 </div>
@@ -90,7 +90,7 @@
 
                 <div class="col-md-4 margin-t30">
                 <div class="portfolio-item">
-                    <a href="https://scontent.fbkk12-1.fna.fbcdn.net/v/t1.0-9/16002731_1508472599180261_5873993211054461631_n.jpg?oh=df8024410976726ee4dec97d81b82286&oe=5B469728" data-lightbox="portfolio">
+                    <a href="../static/img/portfolio/item5.jpg" data-lightbox="portfolio">
                     <img src="../assets/img/portfolio/item5.jpg" alt="">
                     </a>
                 </div>
@@ -98,7 +98,7 @@
 
                 <div class="col-md-4 margin-t30">
                 <div class="portfolio-item">
-                    <a href="https://scontent.fbkk12-1.fna.fbcdn.net/v/t31.0-8/20116844_1445878965478330_4462544605663604442_o.jpg?oh=e5df55ffc3b7ae7bd8e32802313d7026&oe=5B0A0A22" data-lightbox="portfolio">
+                    <a href="../static/img/portfolio/item6.jpg" data-lightbox="portfolio">
                     <img src="../assets/img/portfolio/item6.jpg" alt="">
                     </a>
                 </div>
